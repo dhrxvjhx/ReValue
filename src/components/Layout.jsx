58a3090d3c93e1d01d261e15bd289c681034adee
Dhruv <omdhruvjha@gmail.com>
2026-02-23 02:49:28 +0530

    Remove AnimatePresence to fix double-click navigation issue

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,13 +6,27 @@ function Layout() {
     const location = useLocation()
 
     return (
-        <div className="min-h-[100dvh] bg-[#0B1120] text-white flex flex-col">
+        <div className="
+      min-h-[100dvh]
+      bg-gradient-to-b from-[#0B1120] to-[#0E1629]
+      text-white
+      flex flex-col
+      overflow-x-hidden
+    ">
             <motion.div
                 key={location.pathname}
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.3 }}
-                className="flex-1 px-4 pt-6 pb-28 max-w-md mx-auto w-full"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.2 }}
+                className="
+          flex-1
+          px-4
+          pt-6
+          pb-28
+          max-w-md
+          mx-auto
+          w-full
+        "
             >
                 <Outlet />
             </motion.div>
