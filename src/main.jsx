215ae359c703dea788c265e3c1bad8b7700e406b
Dhruv <omdhruvjha@gmail.com>
2026-02-23 09:51:25 +0530

    Added Firebase configuration file

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import { AppProvider } from "./context/AppContext"
+import { AuthProvider } from "./context/AuthContext"
 import "./index.css"
 import { Toaster } from "react-hot-toast"
 
